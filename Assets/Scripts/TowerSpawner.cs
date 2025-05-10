b78a070fd81768ef8121136ba37bb596f7156cdb
Mozin <temiumaru@naver.com>
2025-05-10 19:13:03 +0900

    16강
    레이저 타워

--- Assets/Scripts/TowerSpawner.cs
+++ Assets/Scripts/TowerSpawner.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TowerSpawner : MonoBehaviour
 {
     [SerializeField]
-    private TowerTemplate towerTemplate;    //타워 정보
+    private TowerTemplate[] towerTemplate;    //타워 정보
     [SerializeField]
     private EnemySpawner enemySpawner;      //적 리스트
     [SerializeField]
@@ -13,9 +13,12 @@ public class TowerSpawner : MonoBehaviour
     private SystemTextViewer systemTextViewer; //시스템 메세지 출력
     private bool isOnTowerButton = false;   //타워 건설 버튼 눌렀는지
     private GameObject followTowerClone = null; //임시 타워 제거를 위한 변수
+    private int towerType;              //타워 속성
 
-    public void ReadyToSpawnTower()
+    public void ReadyToSpawnTower(int type)
     {
+        towerType = type;
+
         //버튼 중복 입력 방지
         if ( isOnTowerButton == true )
         {
@@ -23,7 +26,7 @@ public class TowerSpawner : MonoBehaviour
         }
 
         //타워 건설 가능 여부 확인
-        if (towerTemplate.weapon[0].cost > playerGold.CurrentGold )
+        if (towerTemplate[towerType].weapon[0].cost > playerGold.CurrentGold )
         {
             //건설 불가능 메세지
             systemTextViewer.PrintText(SystemType.Money);
@@ -33,7 +36,7 @@ public class TowerSpawner : MonoBehaviour
         //타워 건설 누름
         isOnTowerButton = true;
         //임시타워 생성
-        followTowerClone = Instantiate(towerTemplate.followTowerPrefab);
+        followTowerClone = Instantiate(towerTemplate[towerType].followTowerPrefab);
         //타워건설 취소 코루틴함수
         StartCoroutine("OnTowerCancelSystem");
     }    
@@ -57,9 +60,9 @@ public class TowerSpawner : MonoBehaviour
 
         isOnTowerButton = false; //버튼 초기화
         tile.IsBuildTower = true;//타일에 건설됨 표시
-        playerGold.CurrentGold -= towerTemplate.weapon[0].cost;//골드 감소
+        playerGold.CurrentGold -= towerTemplate[towerType].weapon[0].cost;//골드 감소
         Vector3 position = tileTransform.position + Vector3.back;                       //선택한 타일의 위치에 건설
-        GameObject clone = Instantiate(towerTemplate.towerPrefab, position, Quaternion.identity);     // ""
+        GameObject clone = Instantiate(towerTemplate[towerType].towerPrefab, position, Quaternion.identity);     // ""
         clone.GetComponent<TowerWeapon>().Setup(enemySpawner,playerGold, tile);//enemy 정보전달
 
         Destroy(followTowerClone);  //임시 타워 삭제
--- Assets/Scripts/TowerWeapon.cs
+++ Assets/Scripts/TowerWeapon.cs
@@ -3,23 +3,38 @@ using System.Collections;
 using UnityEngine.XR;
 using Unity.VisualScripting;
 
-public enum WeaponState { SearchTarget = 0, AttackToTarget}
+public enum WeaponType { Cannon = 0, Laser, }
+public enum WeaponState { SearchTarget = 0, TryAttackCannon, TryAttackLaser, }
 
 public class TowerWeapon : MonoBehaviour
 {
+    [Header("Commons")]
     [SerializeField]
     private TowerTemplate towerTemplate;    //타워 정보
     [SerializeField]
-    private GameObject projectilePrefab; //발사체 프리펩
+    private Transform spawnPoint;   //발사체 생성 위치
     [SerializeField]
-    private Transform spawnpoint; //발사체 생성 위치
-    private int level = 0;          //타워 레벨
-    private WeaponState weaponState = WeaponState.SearchTarget;//타워ㅏ 무기의 상태
-    private Transform attackTarget = null;//공격대상
-    private SpriteRenderer spriteRenderer;  //타워 이미지 변경
-    private EnemySpawner enemySpawner;//게임에 존재하는 적 정보 획득용
-    private PlayerGold playerGold;  //골드 정보
-    private Tile ownerTile;     //배치된 타일정보
+    private WeaponType weaponType;          //무기 속성
+
+    [Header("Cannon")]
+    [SerializeField]
+    private GameObject projectilePrefab; //발사체 프리팹
+
+    [Header("Laser")]
+    [SerializeField]
+    private LineRenderer lineRenderer;      //레이저로 사용되는 선
+    [SerializeField]
+    private Transform hitEffect;            //타격 효과
+    [SerializeField]
+    private LayerMask targetLayer;          //광선에 부딫히는 레이어
+
+    private int level = 0;                                              //타워 레벨
+    private WeaponState weaponState = WeaponState.SearchTarget;         //타워 무기의 상태
+    private Transform attackTarget = null;                              //공격대상
+    private SpriteRenderer spriteRenderer;                              //타워 이미지 변경
+    private EnemySpawner enemySpawner;                                  //게임에 존재하는 적 정보 획득용
+    private PlayerGold playerGold;                                      //골드 정보
+    private Tile ownerTile;                                             //배치된 타일정보
 
     public Sprite TowerSprite => towerTemplate.weapon[level].sprite;
     public float Damage => towerTemplate.weapon[level].damage;
@@ -68,42 +83,32 @@ public class TowerWeapon : MonoBehaviour
     {
         while (true)
         {
-            float closestDistSqr = Mathf.Infinity;
-
-            for (int i = 0; i < enemySpawner.EnemyList.Count; i++)
-            {
-                float distance = Vector3.Distance(enemySpawner.EnemyList[i].transform.position, transform.position);
-
-                if (distance <= towerTemplate.weapon[level].range &&distance <= closestDistSqr) 
-                { 
-                    closestDistSqr = distance;
-                    attackTarget = enemySpawner.EnemyList[i].transform;
-                }
-            }
+            //가장 가까운 적 탐색
+            attackTarget = FindClosestAttackTarget();
 
             if (attackTarget != null) 
             {
-                ChangeState(WeaponState.AttackToTarget);
+                if ( weaponType == WeaponType.Cannon )
+                {
+                    ChangeState(WeaponState.TryAttackCannon);
+                }
+                else if ( weaponType == WeaponType.Laser )
+                {
+                    ChangeState(WeaponState.TryAttackLaser);
+                }
             }
 
             yield return null;
         }
     }
 
-    private IEnumerator AttackToTarget()
+    private IEnumerator TryAttackCannon()
     {
         while (true)
         {
-            if (attackTarget == null)
+            //target 공격 가능한지 검사
+            if ( IsPossibleToAttackTarget() == false )
             {
-                ChangeState(WeaponState.SearchTarget);
-                break;
-            }
-
-            float distance = Vector3.Distance(attackTarget.position, transform.position);
-            if (distance > towerTemplate.weapon[level].range)
-            {
-                attackTarget = null;
                 ChangeState(WeaponState.SearchTarget);
                 break;
             }
@@ -114,11 +119,98 @@ public class TowerWeapon : MonoBehaviour
         }
     }
 
+    private IEnumerator TryAttackLaser()
+    {
+        //레이저 활성화
+        EnableLaser();
+
+        while (true)
+        {
+            //공격 불가능한 경우
+            if (IsPossibleToAttackTarget() == false)
+            {
+                //레이저 비활
+                DisableLaser();
+                ChangeState(WeaponState.SearchTarget);
+                break;
+            }
+
+            //공격
+            SpawnLaser();
+
+            yield return null;
+        }
+    }
+
+    private Transform FindClosestAttackTarget()
+    {
+        //최초거리 크게 설정(최소거리 적 탐색)
+        float closestDistSqr = Mathf.Infinity;
+        //모든 적 검사
+        for (int i = 0; i < enemySpawner.EnemyList.Count; ++i)
+        {
+            float distance = Vector3.Distance(enemySpawner.EnemyList[i].transform.position, transform.position) ;
+            //적이 사정거리 내, 가장 가까울 시
+            if ( distance <= towerTemplate.weapon[level].range && distance < closestDistSqr)
+            {
+                closestDistSqr = distance;
+                attackTarget = enemySpawner.EnemyList[i].transform;
+            }
+        }
+
+        return attackTarget;
+    }
+
+    private bool IsPossibleToAttackTarget()
+    {
+        //타겟 유무 검사
+        if ( attackTarget == null )
+        {
+            return false;
+        }
+
+        return true;
+    }    
+
     private void SpawnProjectile()
     {
-        GameObject clone = Instantiate(projectilePrefab, spawnpoint.position, Quaternion.identity);
+        GameObject clone = Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
 
         clone.GetComponent<Projectile>().Setup(attackTarget, towerTemplate.weapon[level].damage);
+    }
+
+    private void EnableLaser()
+    {
+        lineRenderer.gameObject.SetActive(true);
+        hitEffect.gameObject.SetActive(true);
+    }
+
+    private void DisableLaser()
+    {
+        lineRenderer.gameObject.SetActive(false);
+        hitEffect.gameObject.SetActive(false);
+    }
+
+    private void SpawnLaser()
+    {
+        Vector3 direction = attackTarget.position - spawnPoint.position;
+        RaycastHit2D[] hit = Physics2D.RaycastAll(spawnPoint.position, direction, towerTemplate.weapon[level].range, targetLayer);
+
+        //target에 맞도록 광선 여러개 발사
+        for ( int i = 0; i < hit.Length; i++ )
+        {
+            if (hit[i].transform == attackTarget)
+            { 
+                //선 시작
+                lineRenderer.SetPosition(0, spawnPoint.position);
+                //선 목표
+                lineRenderer.SetPosition(1, new Vector3(hit[i].point.x, hit[i].point.y, 0) + Vector3.back);
+                //타격 효과 위치
+                hitEffect.position = hit[i].point;
+                //적 체력감소
+                attackTarget.GetComponent<EnemyHP>().TakeDamage(towerTemplate.weapon[level].damage * Time.deltaTime);
+            }
+        }
     }
     
     public bool Upgrade()
@@ -135,6 +227,14 @@ public class TowerWeapon : MonoBehaviour
         spriteRenderer.sprite = towerTemplate.weapon[level].sprite;
         //골드 차감
         playerGold.CurrentGold -= towerTemplate.weapon[level].cost;
+
+        //무기가 레이저라면
+        if ( weaponType == WeaponType.Laser )
+        {
+            //굵기 변화
+            lineRenderer.startWidth = 0.05f + level * 0.05f;
+            lineRenderer.endWidth = 0.05f;
+        }    
 
         return true;
     }
